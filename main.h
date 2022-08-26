3c20412c50253c246c595264d53fd83f28d0abfa
marsnebulasoup <marsnebulasoup@gmail.com>
2022-08-26 16:44:43 -0500

    Stubbed out main header and program files

---
+++ main.h
@@ -0,0 +1,15 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Classes and Objects
+Purpose: A refresher assignment on classes and objects.
+**********************/
+
+#ifndef LB_MAIN_H
+#define LB_MAIN_H
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#endif // LB_MAIN_H
