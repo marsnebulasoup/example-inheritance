b4a40fcb19638b9c7523beaaf7a1720fcb56c572
marsnebulasoup <marsnebulasoup@gmail.com>
2022-08-28 10:52:30 -0500

    Implemented SmartBulb class

---
+++ smartbulb.cpp
@@ -0,0 +1,45 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Classes and Objects
+Purpose: A refresher assignment on classes and objects.
+**********************/
+
+#include "smartbulb.h"
+
+SmartBulb::SmartBulb(int wattage, int lumens, int minColor, int maxColor, int lifespan, bool hasBluetooth)
+{
+  setEnergyConsumption(wattage);
+  setBrightness(lumens);
+  setMinColor(minColor);
+  setMaxColor(maxColor);
+  setLifespan(lifespan);
+  setBluetooth(hasBluetooth);
+}
+
+int SmartBulb::getMinColor() {
+  return minColor;
+}
+
+int SmartBulb::getMaxColor() {
+  return maxColor;
+}
+
+bool SmartBulb::getBluetooth() {
+  return hasBluetooth;
+}
+
+void SmartBulb::setMinColor(int minColor)
+{
+  this->minColor = minColor > 0 ? minColor : DEFAULT_MIN_COLOR;
+}
+
+void SmartBulb::setMaxColor(int maxColor)
+{
+  this->maxColor = maxColor > 0 ? maxColor : DEFAULT_MAX_COLOR;
+}
+
+void SmartBulb::setBluetooth(bool hasBluetooth)
+{ // since hasBluetooth is a boolean, there's really no need to validate it, 
+  // but it cannot be modified afterwards without a setter, since it's private.
+  this->hasBluetooth = hasBluetooth;
+}
---
+++ smartbulb.h
@@ -0,0 +1,37 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Classes and Objects
+Purpose: A refresher assignment on classes and objects.
+**********************/
+
+#ifndef LB_CLASS_SMARTBULB
+#define LB_CLASS_SMARTBULB
+
+#include "lightbulb.h"
+
+#define DEFAULT_MIN_COLOR 0
+#define DEFAULT_MAX_COLOR 0
+
+class SmartBulb : public LightBulb
+{
+protected:
+  int minColor; // in Kelvin
+  int maxColor; // in Kelvin
+  bool hasBluetooth;
+
+public:
+  SmartBulb(int, int, int, int, int, bool);
+
+  int getMinColor();
+  int getMaxColor();
+  bool getBluetooth();
+
+  void setMinColor(int);
+  void setMaxColor(int);
+  void setBluetooth(bool);
+
+private:
+  void getColor();
+};
+
+#endif
