7d9b302fe0659ec217fd85d1a6a9bddf34b9e368
marsnebulasoup <marsnebulasoup@gmail.com>
2022-08-28 11:06:11 -0500

    Implemented functions module

---
+++ functions.cpp
@@ -0,0 +1,34 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Classes and Objects
+Purpose: A refresher assignment on classes and objects.
+**********************/
+
+#include "functions.h"
+
+void displayLightBulb(LightBulb &lightbulb)
+{
+  static int currentLightBulb;
+  currentLightBulb++;
+  cout << "Lightbulb " << currentLightBulb << endl;
+  cout << " - Energy consumption: " << lightbulb.getEnergyConsumption() << " W" << endl;
+  cout << " - Brightness: " << lightbulb.getBrightness() << " Lumens" << endl;
+  cout << " - Color: " << lightbulb.getColor() << " K" << endl;
+  cout << " - Lifespan: " << lightbulb.getLifespan() << " hours" << endl;
+  cout << " - Lifetime energy consumption: " << lightbulb.getLifetimeEnergyConsumption() << " kWh" << endl;
+  cout << endl;
+}
+
+void displaySmartBulb(SmartBulb &smartbulb)
+{
+  static int currentSmartBulb;
+  currentSmartBulb++;
+  cout << "Smartbulb " << currentSmartBulb << endl;
+  cout << " - Energy consumption: " << smartbulb.getEnergyConsumption() << " W" << endl;
+  cout << " - Brightness: " << smartbulb.getBrightness() << " Lumens" << endl;
+  cout << " - Min Color: " << smartbulb.getMinColor() << " K" << endl;
+  cout << " - Max Color: " << smartbulb.getMaxColor() << " K" << endl;
+  cout << " - Lifespan: " << smartbulb.getLifespan() << " hours" << endl;
+  cout << " - Lifetime energy consumption: " << smartbulb.getLifetimeEnergyConsumption() << " kWh" << endl;
+  cout << endl;
+}
---
+++ functions.h
@@ -0,0 +1,20 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Classes and Objects
+Purpose: A refresher assignment on classes and objects.
+**********************/
+
+#ifndef LB_FUNCTIONS_H
+#define LB_FUNCTIONS_H
+
+#include "lightbulb.h"
+#include "smartbulb.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void displayLightBulb(LightBulb& lightbulb);
+void displaySmartBulb(SmartBulb& smartbulb);
+
+#endif
