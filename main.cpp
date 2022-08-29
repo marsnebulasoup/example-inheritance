d8aafe3b0907b1c10b31ef53ff2781f0de034cc1
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2022-08-29 14:41:01 -0500

    Added driver code to main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Classes and Objects
+Purpose: A refresher assignment on classes and objects.
+**********************/
+
 #include "main.h"
 
-int main() {
+int main()
+{
+  LightBulb lightbulb1(40, 320, 2600, 1500);
+  LightBulb lightbulb2(-40, -320, -2600, -1500);
+  LightBulb lightbulb3(40, -320, 2600, -1500);
+  LightBulb lightbulb4(0, 0, 0, 0);
+  LightBulb lightbulb5;
+
+  SmartBulb smartbulb1(9, 800, 1000, 5000, 15000, true);
+  SmartBulb smartbulb2(-9, -800, -1000, -5000, -15000, false);
+  SmartBulb smartbulb3(-9, -800, 1000, -5000, 15000, true);
+  SmartBulb smartbulb4(0, 0, 0, 0, 0, true);
+  SmartBulb smartbulb5(0, 0, 0, 0, 0, false);
+
+  displayLightBulb(lightbulb1);
+  displayLightBulb(lightbulb2);
+  displayLightBulb(lightbulb3);
+  displayLightBulb(lightbulb4);
+  displayLightBulb(lightbulb5);
+
+  displaySmartBulb(smartbulb1);
+  displaySmartBulb(smartbulb2);
+  displaySmartBulb(smartbulb3);
+  displaySmartBulb(smartbulb4);
+  displaySmartBulb(smartbulb5);
+  displayLightBulb(smartbulb1);
+
   return 0;
 }
--- main.h
+++ main.h
@@ -7,9 +7,8 @@ Purpose: A refresher assignment on classes and objects.
 #ifndef LB_MAIN_H
 #define LB_MAIN_H
 
-#include <iostream>
-
-using std::cout;
-using std::endl;
+#include "lightbulb.h"
+#include "smartbulb.h"
+#include "functions.h"
 
 #endif // LB_MAIN_H
